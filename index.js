99a058d6dd67ab20736d96b0ceae48494e1a4034
Rohit Kumar <rohit96682@gmail.com>
2023-09-19 17:53:51 +0530

    dynamic rendering

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const http = require("http");
 const fs = require("fs");
 
 const index = fs.readFileSync("index.html", "utf-8");
-const data = fs.readFileSync("db.json", "utf-8");
+const data = JSON.parse(fs.readFileSync("db.json", "utf-8"));
+const product = data.products[0];
 
-const hostname = "127.0.0.1";
+const hostname = "127.0.0.1"; 
 const port = 3000;
 
 const server = http.createServer((req, res) => {
@@ -18,6 +19,13 @@ const server = http.createServer((req, res) => {
     case "/api":
       res.setHeader("Content-Type", "application/json");
       res.end(data);
+      break;
+    case "/products":
+      res.setHeader("Content-Type", "text/html");
+      const modifiedIndex = index
+        .replace("**title**", product.title)
+        .replace("**imgUrl**", product.thumbnail);
+      res.end(modifiedIndex);
       break;
     default:
       res.writeHead(404);
