ae12835c724e689f26f39b8656b9bc10e09d4f94
Rohit Kumar <rohit96682@gmail.com>
2023-09-19 18:07:05 +0530

    server rendered site

--- index.js
+++ index.js
@@ -2,13 +2,25 @@ const http = require("http");
 const fs = require("fs");
 
 const index = fs.readFileSync("index.html", "utf-8");
-const data = fs.readFileSync("db.json", "utf-8");
+const data = JSON.parse(fs.readFileSync("db.json", "utf-8"));
+const products = data.products;
 
 const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
+
+  if (req.url.startsWith("/product")) {
+    const id = req.url.split("/")[2];
+    const product = products.find((p) => p.id === Number(id));
+    res.setHeader("Content-Type", "text/html");
+    const modifiedIndex = index
+      .replace("**title**", product.title)
+      .replace("**imgUrl**", product.thumbnail);
+    res.end(modifiedIndex);
+    return;
+  }
 
   switch (req.url) {
     case "/":
