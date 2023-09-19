bfa7f78219d140634b84d09253af500c30c372a4
Rohit Kumar <rohit96682@gmail.com>
2023-09-19 17:33:13 +0530

    creating route

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const http = require("http");
+const fs = require("fs");
+
+const index = fs.readFileSync("index.html", "utf-8");
+const data = fs.readFileSync("db.json", "utf-8");
 
 const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World\n");
+
+  switch (req.url) {
+    case "/":
+      res.setHeader("Content-Type", "text/html");
+      res.end(index);
+      break;
+    case "/api":
+      res.setHeader("Content-Type", "application/json");
+      res.end(data);
+      break;
+    default:
+      res.writeHead(404);
+      res.end();
+      break;
+  }
 });
 
 server.listen(port, hostname, () => {
