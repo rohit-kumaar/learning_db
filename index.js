3c4a79cc6ec9de0de2339224fc7407a9d8a825dd
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2023-09-20 12:54:21 +0530

    API  or Middleware

--- index.js
+++ index.js
@@ -5,16 +5,42 @@ const products = data.products;
 
 const express = require("express");
 const hostname = "127.0.0.1";
-const port = 3000;
+const port = 8080;
 
 const server = express();
+const morgan = require("morgan");
 
-// API - Endpoint - Route
-server.get("/", (req, res) => {
+// ✅ Built-in middleware
+server.use(express.json()); // bodyParser
+// server.use(express.urlencoded());
+server.use(morgan("default"));
+server.use(express.static("public"));
+
+// ✅ Application-level middleware
+server.use((req, res, next) => {
+  console.log({
+    date: new Date(),
+    method: req.method,
+    ip: req.ip,
+    hostname: req.hostname,
+  });
+  next();
+});
+
+// ✅ Router-level middleware
+const auth = (req, res, next) => {
+  // req.query.password === "123" ? next() : res.sendStatus(401);
+  // req.body.password === "123" ? next() : res.sendStatus(401); // 👈 bcz testing params
+  next();
+};
+
+// ✅ API - Endpoint - Route
+server.get("/product/:id", auth, (req, res) => {
+  console.log(req.params);
   res.json({ type: "GET" });
 });
 
-server.post("/", (req, res) => {
+server.post("/", auth, (req, res) => {
   res.json({ type: "POST" });
 });
 
