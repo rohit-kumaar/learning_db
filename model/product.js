441d183279663c8d803fba51cd8006706d4a410b
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2023-09-22 13:17:11 +0530

    schema validator

--- model/product.js
+++ model/product.js
@@ -2,15 +2,23 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const productSchema = new Schema({
-  title: String,
+  title: { type: String, require: true, unique: true },
   description: String,
-  price: Number,
-  discountPercentage: Number,
-  rating: Number,
+  price: { type: Number, min: [0, "Wrong Price"], required: true },
+  discountPercentage: {
+    type: Number,
+    min: [0, "Wrong min discount"],
+    max: [50, "Wrong max discount"],
+  },
+  rating: {
+    type: Number,
+    min: [0, "Wrong min rating"],
+    max: [5, "Wrong max rating"],
+  },
   stock: Number,
-  brand: String,
-  category: String,
-  thumbnail: String,
+  brand: { type: String, required: true },
+  category: { type: String, required: true },
+  thumbnail: { type: String, required: true },
   images: [String],
 });
 
