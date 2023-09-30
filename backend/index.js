9e30971363f87a53c3d11323ca56793802281262
Rohit Kumar <rohit96682@gmail.com>
2023-09-30 12:48:27 +0530

    final commit

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const server = express();
+const path = require("path");
 
 main().catch((err) => console.log(err));
 async function main() {
@@ -15,11 +16,19 @@ const userRouter = require("./routes/user");
 
 const host = process.env.HOST;
 const port = process.env.PORT;
+const public_dir = process.env.PUBLIC_DIR;
 
 // ✅ Built-in middleware
 server.use(cors());
-server.use(express.json()); // bodyParser
+server.use(express.json()); // ✅ bodyParser
+server.use(express.static(path.resolve(__dirname, public_dir)));
 
+// ✅ This is not working, bcz i set proper router in React App, in Tutorial Doesn't set the proper path in React App
+// server.use("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "build", "index.html"));
+// });
+
+// ✅ Routers paths
 server.use("/products", productRouter);
 server.use("/users", userRouter);
 
